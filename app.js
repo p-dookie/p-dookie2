07a31788efe3da650d7fed3a092830beef29d966
Colm Henry <colm.henry@hotmail.com>
2020-06-10 10:58:56 -0700

    All files converted to ejs. Everything apears to be working as normal. I have done significant testing and all apears to be working as it did before the conversion. no bugs

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const bodyParser = require("body-parser")
 
 const app = express();
 
+const fs = require("fs");
 app.set('view engine', 'ejs');
-
-const https = require("https");
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -24,41 +23,37 @@ app.get("/tanks", function(req, res) {
 });
 
 app.get("/pricing", function(req, res) {
-  res.sendFile(__dirname + "/pricing.html");
+  res.render("signup");
 });
 
 app.get("/pager", function(req, res) {
-  res.sendFile(__dirname + "/user-page.html");
+  res.render("user-page");
 });
 
 app.get("/signin", function(req, res) {
-  res.sendFile(__dirname + "/sign-in.html");
+  res.render("signin");
 });
 
 app.get("/ifv", function(req, res) {
-  res.sendFile(__dirname + "/IFV.html");
+  res.render("ifv");
 });
 
 app.get("/armouredcars", function(req, res) {
-  res.sendFile(__dirname + "/arm-cars.html");
+  res.render("armcars");
 });
 
-app.get("/featured", function(req, res) {
-  res.sendFile(__dirname + "/featured.html");
-});
 
 app.post("/signin", function(req, res) {
   var one = req.body.first;
   var two = req.body.second;
-  var result = "Your email is: " + one + "<br>Your password is: " + two;
-  console.log(result);
-  fs.readFile(__dirname + "/singedin.html", (err, data) => {
-    res.send(data.toString().replace('<--!result-->', result));
+  res.render("reked", {
+    rickSult: one,
+    ronSult: two
   });
 });
 
 app.use(function(req, res, next) {
-  res.status(404).sendFile(__dirname + "/failure.html");
+  res.status(404).render("failure");
 });
 
 app.listen(process.env.PORT || 3000, function() {
