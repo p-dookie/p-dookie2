f8672fa7777a82d12e740cdb5de4100f034329e4
Colm Henry <colm.henry@hotmail.com>
2020-08-17 16:54:25 -0700

    Bug fixes continueing, I have no idea why it is refusing to talk kindly to the database

--- app.js
+++ app.js
@@ -60,9 +60,10 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect(process.env.URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true
 });
+
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
