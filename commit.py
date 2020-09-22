cfd97587b734c6eb65b847c4d577313145fec3ff
Colm Henry <colm.henry@hotmail.com>
2020-09-22 15:27:06 -0700

    images - JUST A TEST

--- commit.py
+++ commit.py
@@ -2,6 +2,6 @@ import os
 from datetime import date
 
 os.system("git add .")
-os.system(f"git commit -m 'images - day{date.today()}'")
+os.system(f'git commit -m "images - JUST A TEST"')
 
 print("DONE")
