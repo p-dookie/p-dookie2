2f19d39df09fbc08d47a9a0a95533bc3db43be94
Colm Henry <colm.henry@hotmail.com>
2020-09-22 15:38:08 -0700

    images - 2020-09-22

--- commit.py
+++ commit.py
@@ -2,6 +2,6 @@ import os
 from datetime import date
 
 os.system("git add .")
-os.system(f'git commit -m "images - {date.today}"')
+os.system(f'git commit -m "images - {date.today()}"')
 
 print("DONE")
