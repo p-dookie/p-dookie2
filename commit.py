1dda6e4ef27f9d97b33b352552d021bce6d882f7
Colm Henry <colm.henry@hotmail.com>
2020-09-23 17:25:06 -0700

    images - 2020-09-23

--- commit.py
+++ commit.py
@@ -2,7 +2,7 @@ import os
 from datetime import date
 
 os.system("git add .")
-os.system(f"git commit -m 'images - day{date.today()}'")
+os.system(f"git commit -m 'images - {date.today()}'")
 os.system("git push heroku master")
 
 print("DONE")
